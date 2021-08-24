cdfdd829bf24d58a926d43f4dbc26d303868f6d6
michaelpfaller <michaelpfaller@web.de>
2021-08-24 15:19:16 +0200

    Add files via upload

--- covax.py
+++ covax.py
@@ -38,7 +38,7 @@ def sql(str_sql,str_con):
     except:
         raise
 
-week = st.radio('Week: ', ('33', '34'))
+week = str(st.slider ("Week: ", min_value=33, max_value=34, value=34, step=1)
 
 df_states = sql("SELECT * from covax WHERE Week = " + week, con())
 
